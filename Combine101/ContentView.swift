b505aa2729a8f46471d60bd4baa93c7deb060264
Paul Solt <PaulSolt@gmail.com>
2019-09-07 17:51:08 -0400

    Playing

--- Combine101/ContentView.swift
+++ Combine101/ContentView.swift
@@ -7,10 +7,81 @@
 //
 
 import SwiftUI
+import Combine
+
+//override func viewDidLoad() {
+//    super.viewDidLoad()
+//
+//    print("Don't forget to call super!")
+//}
+//
+//Text("Hello World")
+//    .onAppear {
+//    print("Only my code here")
+//}
+
+
 
 struct ContentView: View {
+    @State private var text: String = "Swift"
+    
+    var timer: AnyCancellable? = nil
+    
+    mutating func buttonPressed() {
+        
+    }
+    
     var body: some View {
-        Text("Hello World")
+        VStack {
+            Text(text)
+            Button(action: {
+                
+                _ = Timer.publish(every: 0.2, on: .main, in: .default)
+                    .sink {
+                        print($0)
+                }
+//                    .combineLatest("Hello world!".publisher)
+//                    .map { _, character in
+//                        print(character)
+//                        return String(character)
+//                }.assign(to: \.text, on: self)
+                
+                
+                _ = "Hello world!"
+                    .publisher
+                    .map { String($0) }
+                    .sink(receiveValue: { letter in
+                        print(letter)
+                    })
+                        
+//                    .assign(to: \.text, on: self)
+
+            }, label: {
+                Text("Push Me")
+            })
+        }.onAppear(perform: viewDidAppear)
+    }
+    
+    func viewDidAppear() {
+        print("Hello World!")
+        // Experiment here
+        
+        _ = ["Hello", "World"]
+            .publisher
+            .sink(receiveCompletion: { (status) in
+                print("status: \(status)")  // called when finished "iterating" array
+            }) { (word) in
+                print(word) // called for each element in the array
+        }
+            
+            // Does not match type! (Character != String)
+            //        _ = "Hello world!"
+            //            .publisher
+            ////            .delay(for: 1000, scheduler: RunLoop.main)
+            ////            .debounce(for: 1000, scheduler: RunLoop.main)
+            //            .map({ String($0) })
+            
+        
     }
 }
 
@@ -19,3 +90,5 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+
